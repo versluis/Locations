947ee2103d34743bb6c4432c05aa6918fc878bbd
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-01-10 09:32:45 -0500

    Initial Commit

---
+++ Locations/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Locations
+//
+//  Created by Jay Versluis on 10/01/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LocationsTests/LocationsTests.m
@@ -0,0 +1,34 @@
+//
+//  LocationsTests.m
+//  LocationsTests
+//
+//  Created by Jay Versluis on 10/01/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LocationsTests : XCTestCase
+
+@end
+
+@implementation LocationsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
