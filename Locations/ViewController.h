3aa749cfa91a5db5d095586799a5756071f5f615
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-01-10 09:59:15 -0500

    setup manager and delegate, linked framework this should work but it doesn’t no surprises there.

--- Locations/ViewController.h
+++ Locations/ViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CLLocationManagerDelegate>
 
 @end
--- Locations/ViewController.m
+++ Locations/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UILabel *longLabel;
+@property (strong, nonatomic) IBOutlet UILabel *latLabel;
+@property (strong, nonatomic) IBOutlet UILabel *accuracyLabel;
 @end
 
 @implementation ViewController
@@ -17,13 +20,44 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	[self startLocations];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)startLocations {
+    
+    // create and start the location manager
+    CLLocationManager *manager = [[CLLocationManager alloc]init];
+    manager.delegate = self;
+    manager.desiredAccuracy = kCLLocationAccuracyBest;
+    [manager startUpdatingLocation];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"yellow"]];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"blue"]];
+    
+    // grab current location and display it in a label
+    CLLocation *currentLocation = [locations lastObject];
+    
+    NSString *longText = [NSString stringWithFormat:@"%f", currentLocation.coordinate.longitude];
+    NSString *latText = [NSString stringWithFormat:@"%f", currentLocation.coordinate.latitude];
+    NSString *accuracy = [NSString stringWithFormat:@"%f", currentLocation.horizontalAccuracy];
+    
+    self.longLabel.text = longText;
+    self.latLabel.text = latText;
+    self.accuracyLabel.text = accuracy;
+    
 }
 
 @end
